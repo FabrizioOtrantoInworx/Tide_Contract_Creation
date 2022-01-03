a45f32f0fd918af6bf2e821d85f534c429de379f
FabrizioOtrantoInworx <fabrizio.otranto@inworx.com>
2022-01-03 12:32:21 -0300

    Assertions in TestSendJson were updated

--- Test/TestSendJson.py
+++ Test/TestSendJson.py
@@ -18,9 +18,11 @@ class TestSendJson(IsolatedAsyncioTestCase):
         t = open('C:/PythonAutomation/Scripts/Data/SendJson/PastVersionUpdateTemplate.json')
         dataTemplate = json.load(t)
         data = json.load (f)
-        data['LloydsContractRef'] = Utilidades.CreateSourceSystemReference("Source")
+        data['LloydsContractRef'] = Utilidades.CreateSourceSystemReference()
         data['UMR'] = Utilidades.CreateUMR()
         data['VersionUpdatedDate'] = dataTemplate['VersionUpdatedDate']
+        UpdateVersionTime = data['VersionUpdatedDate']
+        UpdateVersionTimeCutted = UpdateVersionTime[0:19]
         data = json.dumps(data)
         writableFile = open('C:/PythonAutomation/Scripts/Data/SendJson/data.json','w')
         writableFile.write(data)
@@ -29,9 +31,9 @@ class TestSendJson(IsolatedAsyncioTestCase):
         url = Properties.Url
         headers = Properties.Header
         r =  requests.post(url, files={'file': open('C:/PythonAutomation/Scripts/Data/SendJson/data.json', 'rb')},headers=headers) 
-        self.assertEqual(r.status_code, 400, 'contract has not been created')
-        self.assertEqual(r.reason,'Bad Request', "contract has not been created")
-        
+        self.assertEqual(r.status_code, 400, 'Bad request was not the reason')
+        self.assertEqual(r.reason,'Bad Request', "Bad request was not the reason")
+        self.assertIn(UpdateVersionTimeCutted, r.text, "Update version time is not in the response text")        
 
 
     def TestFutureVersionUpadte(self):
@@ -39,9 +41,11 @@ class TestSendJson(IsolatedAsyncioTestCase):
         t = open('C:/PythonAutomation/Scripts/Data/SendJson/FutureVersionUpdateTemplate.json')
         dataTemplate = json.load(t)
         data = json.load (f)
-        data['LloydsContractRef'] = Utilidades.CreateSourceSystemReference("Source")
+        data['LloydsContractRef'] = Utilidades.CreateSourceSystemReference()
         data['UMR'] = Utilidades.CreateUMR()
         data['VersionUpdatedDate'] = dataTemplate['VersionUpdatedDate']
+        UpdateVersionTime = data['VersionUpdatedDate']
+        UpdateVersionTimeCutted = UpdateVersionTime[0:19]
         data = json.dumps(data)
         writableFile = open('C:/PythonAutomation/Scripts/Data/SendJson/data.json','w')
         writableFile.write(data)
@@ -50,18 +54,16 @@ class TestSendJson(IsolatedAsyncioTestCase):
         url = Properties.Url
         headers = Properties.Header
         r =  requests.post(url, files={'file': open('C:/PythonAutomation/Scripts/Data/SendJson/data.json', 'rb')},headers=headers) 
-        self.assertEqual(r.status_code, 400, 'contract has not been created')
-        self.assertEqual(r.reason,'Bad Request', "contract has not been created")
-        
-        
+        self.assertEqual(r.status_code, 400, 'Bad request was not the reason')
+        self.assertEqual(r.reason,'Bad Request', "Bad request was not the reason")
+        self.assertIn(UpdateVersionTimeCutted, r.text, "Update version time is not in the response text")        
 
     def TestVersionUpdateDateEqualCurrentDataTime(self):  
         now = datetime.datetime.now()
         NVUD = now.strftime('%Y-%m-%dT%H:%M:%S.000Z')
         f = open('C:/PythonAutomation/Scripts/Data/SendJson/data.json')
-        t = open('C:/PythonAutomation/Scripts/Data/SendJson/template.json')
         data = json.load (f)
-        data['LloydsContractRef'] = Utilidades.CreateSourceSystemReference("Source")
+        data['LloydsContractRef'] = Utilidades.CreateSourceSystemReference()
         data['UMR'] = Utilidades.CreateUMR()
         data['VersionUpdatedDate'] = NVUD
         data = json.dumps(data)
@@ -72,7 +74,7 @@ class TestSendJson(IsolatedAsyncioTestCase):
         url = Properties.Url
         headers = Properties.Header
         r =  requests.post(url, files={'file': open('C:/PythonAutomation/Scripts/Data/SendJson/data.json', 'rb')},headers=headers) 
-        self.assertEqual(r.status_code, 200, 'contract has been created')
-        self.assertEqual(r.reason,'OK', "contract has been created")
+        self.assertEqual(r.status_code, 200, 'contract has not been created')
+        self.assertEqual(r.reason,'OK', "contract has not been created")
         
 
